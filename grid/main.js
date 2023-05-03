c03b792897b1e2d65171c8f50e2b3d2eb2ddca75
Rahul Kumar <rkumar3@rkumar373M7L.vmware.com>
2023-05-03 16:56:06 +0530

    Grid in CSS

---
+++ grid/main.js
@@ -0,0 +1,7 @@
+const switcher = document.getElementById("switcher");
+const root = document.documentElement;
+
+switcher.addEventListener("change", function (evt) {
+  root.style.setProperty("--var-col", evt.target.value);
+});
+
---
+++ grid/main4.js
@@ -0,0 +1,8 @@
+const switcher = document.getElementById("switcher");
+const root = document.documentElement;
+
+switcher.addEventListener("change", function (evt) {
+  root.style.setProperty("--var-repeat", evt.target.value);
+});
+
+
